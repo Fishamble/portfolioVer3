1f12e6586c5397e5fe66d078199f574f7f05dc59
john Paul Larkin <johnplarkin@gmail.com>
2023-08-07 22:47:43 +0100

    other projects layout

--- src/components/jpmazon/Jpmazon.tsx
+++ src/components/jpmazon/Jpmazon.tsx
@@ -7,14 +7,13 @@ import "./jpmazon.css";
 export default function Jpmazon() {
   return (
     <motion.aside initial={initialLeft} whileInView={animate} viewport={{ once: true }} className="other-projects-container jpmazon">
-      <div className="op-description">
-        <h2 className="project-heading">JPMazon</h2>
+      <h2>JPMazon</h2>
+      <div className="op-flex-wrap">
         <p>
           An amazon clone built using react. I used dummyjson.com, which provides fake product data and images through REST endpoints, to create
           responsive product pages and a basic shopping cart.
         </p>
-      </div>
-      <div>
+
         <img width="500" height="406" src={JpMazon} alt="landing page for the jpmazon project" />
       </div>
     </motion.aside>
--- src/components/jsQuestions/Jsquestions.tsx
+++ src/components/jsQuestions/Jsquestions.tsx
@@ -1,19 +1,18 @@
 import { motion } from "framer-motion";
 import jsQuestions from "../../assets/images/questionsApp.webp";
 import { animate, initialRight } from "../../misc/animateValues";
-import './jsQuestions.css';
+import "./jsQuestions.css";
 
 export default function Jsquestions() {
   return (
     <motion.aside initial={initialRight} whileInView={animate} viewport={{ once: true }} className="other-projects-container js-questions">
-      <div className="op-description">
-        <h2>Coding questions</h2>
+      <h2>Coding questions</h2>
+      <div className="op-flex-wrap">
         <p>
           An app for testing coding related questions. I used react for the front end and firestore for the back. Aloing the way I implemented the
           react context API for state management and the intersection observer API for infinite scrolling.
         </p>
-      </div>
-      <div>
+
         <img width="500" height="406" src={jsQuestions} alt="landing page for the coding questions app" />
       </div>
     </motion.aside>
